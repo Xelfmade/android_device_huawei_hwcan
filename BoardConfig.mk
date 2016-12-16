2ba5f9ae88593477e286fff3d8b0b6963b5dcd2b
Felix Matthies <matthiesfx@gmail.com>
2016-12-16 22:39:21 +0100

    fstab changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,7 +49,6 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 RECOVERY_SDCARD_ON_DATA := true
 TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_INCLUDE_CRYPTO := true
 TW_THEME := portrait_hdpi
 
 
