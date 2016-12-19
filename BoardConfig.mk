a7b0dc3205efe61b791577b4af402da209f953d1
Felix Matthies <matthiesfx@gmail.com>
2016-12-19 11:31:46 +0100

    fstab adjustments

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 # TWRP
 BOARD_SUPPRESS_SECURE_ERASE := true
 RECOVERY_SDCARD_ON_DATA := true
-#TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_THEME := portrait_hdpi
 
